41c60e5cf78b8027dd6b3822d54b62e816bb623b
Naveen <naveenkrishnawodeyar@gmail.com>
2023-10-15 16:38:24 +0530

    Logger log = LogManager.getLogger(Test.class);

--- src/main/java/com/log/Test.java
+++ src/main/java/com/log/Test.java
@@ -1,8 +1,23 @@
 package com.log;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Test 
 {
-	private final static 
+	private final static Logger log = LogManager.getLogger(Test.class);
+	
+	static public  void getLog()
+	{
+		System.out.println("\n Logging levels,");
+		log.trace("\nFrom trace(),");
+		log.info("\nFrom info(),");
+		log.debug("\nFrom debug(),");
+		log.warn("\nFrom warn(),");
+		log.fatal("\nFrom fatal(),");
+		log.error("\nFfom error(),");
+	}
+	
 	public static void main(String[] args) 
 	{
 		System.out.println("\nWelcome");
