3f6b0df08046bfe2f62ecf61f3a5e0a87fa05dab
Naveen <naveenkrishnawodeyar@gmail.com>
2023-10-16 15:42:45 +0530

    private static Calci calci = null;

--- src/test/java/com/junit/CalciTest.java
+++ src/test/java/com/junit/CalciTest.java
@@ -2,18 +2,33 @@ package com.junit;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.juint.Calci;
 
 public class CalciTest 
 {
+	private static Calci calci = null;
+	
+	@BeforeClass
+	public static void beforeAll()
+	{
+	// Object creation,
+		calci = new Calci();
+	}
+	
+	@AfterClass
+	public static void  afterAll()
+	{
+		calci = null;
+	}
+	
+	// 1. for testing add();
 	@Test
 	public void testAdd()
-	{	
-		// Object creation,
-		var calci = new Calci();
-		
+	{			
 		int actual = calci.add(1, 1);
 		int expected = 2;
 		
@@ -21,14 +36,12 @@ public class CalciTest
 		assertEquals(expected, actual);
 	}
 	
-	// for testing sub(),
+	// 2. for testing sub(),
 	@Test
 	public void testSub()
-	{
-		var calci = new Calci();
-		
+	{		
 		int actual = calci.sub(1, 1);
-		int expected = 01;
+		int expected = 0;
 		
 		assertEquals(expected, actual);
 	}
