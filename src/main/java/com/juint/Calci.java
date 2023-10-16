7302772edea31198e39bc0c6cc2949dae9eadccf
Naveen <naveenkrishnawodeyar@gmail.com>
2023-10-16 15:12:42 +0530

    manually tested the methods,

---
+++ src/main/java/com/juint/Calci.java
@@ -0,0 +1,28 @@
+package com.juint;
+
+public class Calci 
+{
+	public int add(int a, int b)
+	{
+		System.out.println("\n****Addition******");
+		return a+b;
+	}
+	
+	public int sub(int a, int b)
+	{
+		System.out.println("\n****Substraction******");
+		return a-b;
+	}
+	
+	public int mult(int a, int b)
+	{
+		System.out.println("\n****Product******");
+		return a*b;
+	}
+	
+	public int div(int a, int b)
+	{
+		System.out.println("\n****Division******");
+		return a/b;
+	}
+}
---
+++ src/main/java/com/juint/TestClass.java
@@ -0,0 +1,28 @@
+package com.juint;
+
+public class TestClass
+{
+
+	public static void main(String[] args) 
+	{
+		var c = new Calci();
+		
+		// test the methods manually,
+		int a = 2;
+		int b = 2;
+		
+		System.out.println("Nmbers for the calculation,"+a+" and "+b);
+		int sum = c.add(a, b);		// 4
+		System.out.println("\n Sum of numbers:"+sum);
+		
+		int diff = c.sub(a, b);		// 0
+		System.out.println("\n Difference of numbers:"+diff);
+		
+		int prod = c.mult(a, b);	// 4
+		System.out.println("\n Product of numbers:"+prod);
+		
+		int div = c.div(a, b);		// 1
+		System.out.println("\n Division of numbers:"+div);
+	}
+
+}
