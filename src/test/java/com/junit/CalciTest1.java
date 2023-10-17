586313460153ae04b50cb143089b8101e1bbf5f2
Naveen <naveenkrishnawodeyar@gmail.com>
2023-10-17 11:52:30 +0530

    GM

--- src/test/java/com/junit/CalciTest1.java
+++ src/test/java/com/junit/CalciTest1.java
@@ -21,13 +21,23 @@ public class CalciTest1
 		assertEquals(expected, actual);
 	}
 	
-	// 2. for testing sub(),
+	// 2. for testing mult(),
 	@Test
-	public void testSub()
+	public void tesMulti()
 	{		
-		int actual = calci.sub(1, 1);
+		int actual = calci.mult(1, 1);
 		int expected = 0;
 		
 		assertEquals(expected, actual);
 	}
+	
+	// 3. for testing sub(),
+		@Test
+		public void testSub()
+		{		
+			int actual = calci.sub(1, 1);
+			int expected = 0;
+			
+			assertEquals(expected, actual);
+		}
 }
