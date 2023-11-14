73607a998507f937762ee699fee953d764ff553d
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-14 22:15:21 +0530

    logOuT

--- src/main/java/com/juint/Calci.java
+++ src/main/java/com/juint/Calci.java
@@ -4,25 +4,25 @@ public class Calci
 {
 	public int add(int a, int b)
 	{
-		System.out.println("\n****1.Addition******");
+		System.out.println("\n****1.Addition******\n");
 		return a+b;
 	}
 	
 	public int sub(int a, int b)
 	{
-		System.out.println("\n****2.Substraction******");
+		System.out.println("\n****2.Substraction******\n");
 		return a-b;
 	}
 	
 	public int mult(int a, int b)
 	{
-		System.out.println("\n****3.multiplication******");
+		System.out.println("\n****3.multiplication******\n");
 		return a*b;
 	}
 	
 	public float div(int a, int b)
 	{
-		System.out.println("\n****4.Division******");
+		System.out.println("\n****4.Division******\n");
 		return a/b;
 	}
 	
